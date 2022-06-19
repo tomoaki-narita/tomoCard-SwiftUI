a758cae89a2c41d62728a6bc9f0ad82b8ccfb53b
tomoaki-narita <output.org@gmail.com>
2022-06-19 20:38:33 +0900

    Initial Commit

---
+++ tomoCard/tomoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomoCardApp.swift
+//  tomoCard
+//
+//  Created by output. on 2022/06/19.
+//
+
+import SwiftUI
+
+@main
+struct tomoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
